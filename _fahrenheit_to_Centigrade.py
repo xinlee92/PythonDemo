e4ec4157ecf4533d6da0f92105138bf9531acd70
Li Xin <xinlee92@163.com>
2016-09-10 21:49:50 +0800

    _fahrenheit_to_Centigrade
    #This program transforms the fahrenheit 
    #degree the user input into centigrade degree

---
+++ _fahrenheit_to_Centigrade.py
@@ -0,0 +1,12 @@
+#1-2_fahrenheit_to_Centigrade.py
+#This program transforms the fahrenheit 
+#degree the user input into centigrade degree
+
+#get fahrenheit degree
+F=int(input('Fahrenheit? '))
+
+#transform into centigrade degree
+c=5/9*(F-32)
+
+#print the results
+print('The centigrade degree is '+str(c))
